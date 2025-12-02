7450d8c33bffca31055da1e6726ca95489b46c3c
Aswin Vijayakumar <aswinvk28@gmail.com>
2025-12-02 15:39:54 +0000

    Refactor Firebase config and initialization logic
    Updated firebase configuration with hardcoded values and refactored app initialization logic.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,33 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDr38KEY4HwAnx57SMDMwntDOGRfpnA_3o",
+  authDomain: "tasktracktwo.firebaseapp.com",
+  projectId: "tasktracktwo",
+  storageBucket: "tasktracktwo.firebasestorage.app",
+  messagingSenderId: "857732232066",
+  appId: "1:857732232066:web:0cfe2c0ad86a2f1fbb959b"
 };
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+let app = null;
+
+if(!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
+
+// const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 
 export { db };
